8405d2b8417ae1f6e4905c597c66e3f439eedf3f
Zimil-Patel <patelzimil2310@gmail.com>
2024-04-30 21:46:40 +0530

    30-04-2024

--- lib/intro using shared preferences/screens/intro screen/view/components/bottom_button_row.dart
+++ lib/intro using shared preferences/screens/intro screen/view/components/bottom_button_row.dart
@@ -17,7 +17,7 @@ class BottomButtonRow extends StatelessWidget {
     int index =
         Provider.of<IntroScreenProvider>(context, listen: true).getIndex();
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      padding: const EdgeInsets.only(bottom: 30.0),
       child: Align(
         alignment: Alignment.bottomCenter,
         child: index == introList.length - 1
@@ -38,31 +38,30 @@ class LetsGoButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
-        padding: EdgeInsets.zero,
-        onPressed: () {
-          Provider.of<IntroScreenProvider>(context, listen: false).setPreferenceToTrue();
-          Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const FoodExpressHomeScreen(),
-              ));
-        },
-        child: Container(
-          padding:
-              const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius:
-                  const BorderRadius.all(Radius.circular(30))),
-          child: Text(
-            'Let\'s Go',
-            style: Theme.of(context)
-                .textTheme
-                .bodyMedium!
-                .copyWith(color: Colors.white),
-          ),
+      padding: EdgeInsets.zero,
+      onPressed: () {
+        Provider.of<IntroScreenProvider>(context, listen: false)
+            .setPreferenceToTrue();
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const FoodExpressHomeScreen(),
+            ));
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        decoration: BoxDecoration(
+            color: primaryColor,
+            borderRadius: const BorderRadius.all(Radius.circular(30))),
+        child: Text(
+          'Let\'s Go',
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium!
+              .copyWith(color: Colors.white),
         ),
-      );
+      ),
+    );
   }
 }
 
@@ -77,56 +76,57 @@ class SkipAndNextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // SKIP BUTTON
-          CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              Provider.of<IntroScreenProvider>(context, listen: false).setIntroStackIndex(introList.length - 1);
-            },
-            child: Container(
-              padding: const EdgeInsets.only(
-                  left: 24, right: 30, top: 10, bottom: 10),
-              decoration: BoxDecoration(
-                  color: secondaryColor,
-                  borderRadius: const BorderRadius.horizontal(
-                      right: Radius.circular(30))),
-              child: Text(
-                'Skip',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: lableTextColor),
-              ),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // SKIP BUTTON
+        CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            Provider.of<IntroScreenProvider>(context, listen: false)
+                .setIntroStackIndex(introList.length - 1);
+          },
+          child: Container(
+            padding:
+                const EdgeInsets.only(left: 24, right: 30, top: 10, bottom: 10),
+            decoration: BoxDecoration(
+                color: secondaryColor,
+                borderRadius:
+                    const BorderRadius.horizontal(right: Radius.circular(30))),
+            child: Text(
+              'Skip',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: lableTextColor),
             ),
           ),
-          // NEXT BUTTON
-          CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              if (index < introList.length - 1) {
-                Provider.of<IntroScreenProvider>(context, listen: false)
-                    .incrementIndex();
-              }
-            },
-            child: Container(
-              padding: const EdgeInsets.only(
-                  left: 30, right: 24, top: 10, bottom: 10),
-              decoration: BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: const BorderRadius.horizontal(
-                      left: Radius.circular(30))),
-              child: Text(
-                'Next',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: Colors.white),
-              ),
+        ),
+        // NEXT BUTTON
+        CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            if (index < introList.length - 1) {
+              Provider.of<IntroScreenProvider>(context, listen: false)
+                  .incrementIndex();
+            }
+          },
+          child: Container(
+            padding:
+                const EdgeInsets.only(left: 30, right: 24, top: 10, bottom: 10),
+            decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius:
+                    const BorderRadius.horizontal(left: Radius.circular(30))),
+            child: Text(
+              'Next',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.white),
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
