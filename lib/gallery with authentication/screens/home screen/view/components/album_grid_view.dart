ccb42d3481f88505b09188d9d0573dcd49e9fb27
Zimil-Patel <patelzimil2310@gmail.com>
2024-05-19 01:11:31 +0530

    19-05-2024

--- lib/gallery with authentication/screens/home screen/view/components/album_grid_view.dart
+++ lib/gallery with authentication/screens/home screen/view/components/album_grid_view.dart
@@ -1,6 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../provider/gallery_provider.dart';
+
+final DecorationTween _tween = DecorationTween(
+  begin: BoxDecoration(
+    color: CupertinoColors.systemYellow,
+    boxShadow: const <BoxShadow>[],
+    borderRadius: BorderRadius.circular(20.0),
+  ),
+  end: BoxDecoration(
+    color: CupertinoColors.systemYellow,
+    boxShadow: CupertinoContextMenu.kEndBoxShadow,
+    borderRadius: BorderRadius.circular(20.0),
+  ),
+);
 
 class AlbumsGridView extends StatelessWidget {
   const AlbumsGridView({
@@ -9,6 +24,19 @@ class AlbumsGridView extends StatelessWidget {
   });
 
   final GalleryProvider provider;
+
+  static Animation<Decoration> _boxDecorationAnimation(
+      Animation<double> animation) {
+    return _tween.animate(
+      CurvedAnimation(
+        parent: animation,
+        curve: Interval(
+          0.0,
+          CupertinoContextMenu.animationOpensAt,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +49,71 @@ class AlbumsGridView extends StatelessWidget {
       ),
       itemBuilder: (context, index) => Column(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width / 3 - 12 * 2, // Adjust this calculation based on crossAxisCount and crossAxisSpacing
-            height: MediaQuery.of(context).size.width / 3 - 12 * 2, // Make the container square
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              shape: BoxShape.rectangle,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(provider.albumsList[index].img!),
-              ),
-              borderRadius: BorderRadius.circular(20),
+          SizedBox(
+            height: MediaQuery.of(context).size.width / 3 - 12 * 2,
+            width: MediaQuery.of(context).size.width / 3 - 12 * 2,
+            child: CupertinoContextMenu.builder(
+              actions: [
+                const CupertinoContextMenuAction(
+                  isDefaultAction: false,
+                  trailingIcon: Icons.copy,
+                  child: Text('Copy'),
+                ),
+                CupertinoContextMenuAction(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  trailingIcon: CupertinoIcons.share,
+                  child: const Text('Share'),
+                ),
+                const CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.heart,
+                  child: Text('Favorite'),
+                ),
+                const CupertinoContextMenuAction(
+                  isDestructiveAction: true,
+                  trailingIcon: CupertinoIcons.delete,
+                  child: Text('Delete'),
+                ),
+              ],
+              builder: (BuildContext context, Animation<double> animation) {
+                final Animation<Decoration> boxDecorationAnimation =
+                    _boxDecorationAnimation(animation);
+
+                return Container(
+                  decoration:
+                      animation.value < CupertinoContextMenu.animationOpensAt
+                          ? boxDecorationAnimation.value
+                          : null,
+                  child: Container(
+                    width:
+                        300, // Adjust this calculation based on crossAxisCount and crossAxisSpacing
+                    height: 500, // Make the container square
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(provider.albumsList[index].img!),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
           Column(
             children: [
-              Text(provider.albumsList[index].name!, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
-              Text('${provider.albumsList[index].length}', style: const TextStyle(color: Colors.grey),),
+              Text(
+                provider.albumsList[index].name!,
+                style: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '${provider.albumsList[index].length}',
+                style: const TextStyle(color: Colors.grey),
+              ),
             ],
           ),
         ],
